c1b5cb3ec44ea2b2649dedbd611d027ba4c58d62
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2023-07-22 11:41:18 +0500

    some changes in dapartments pages

--- src/views/about/dep_employe/itTechnology.vue
+++ src/views/about/dep_employe/itTechnology.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
 import BaseCreatePage from '@/components/BaseCreatePage/index.vue'
 import { usedepEmploye } from '@/views/about/dep_employe/composable'
+import BaseCard from '@/components/BaseCard/index.vue'
 const { depEmployes } = usedepEmploye()
+const boss = {
+  lavozim: "O‘quv-uslubiy bo‘lim boshlig'i",
+  url: '/images/user.jpg',
+  name: 'Firstname Lastname',
+  phone: '97-000-00-00',
+  email: 'example@gmail.com'
+}
 </script>
 <template>
-  <BaseCreatePage :page-info="depEmployes[3]" info-name="Bo'lim" />
+  <BaseCreatePage :page-info="depEmployes[3]" info-name="Bo'lim">
+    <template #boss>
+      <BaseCard :card-data="boss" />
+    </template>
+  </BaseCreatePage>
 </template>
--- src/views/about/division/energy.vue
+++ src/views/about/division/energy.vue
@@ -2,9 +2,19 @@
 import BaseCreatePage from '@/components/BaseCreatePage/index.vue'
 import { useDivision } from '@/views/about/division/composable'
 const { devisions } = useDivision()
+import BaseCard from '@/components/BaseCard/index.vue'
+const boss = {
+  lavozim: "Infrastruktura bo'limi boshlig'i",
+  url: '/images/user.jpg',
+  name: 'Firstname Lastname',
+  phone: '97-000-00-00',
+  email: 'example@gmail.com'
+}
 </script>
 <template>
   <div>
-    <BaseCreatePage :page-info="devisions[1]"/>
+    <BaseCreatePage :page-info="devisions[1]">
+      <template #boss> <BaseCard :card-data="boss" /></template
+    ></BaseCreatePage>
   </div>
 </template>

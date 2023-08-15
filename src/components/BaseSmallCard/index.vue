f642dd15fe44ae0a2765d68912027abb16f117ad
unknown <you@example.comhusniddinxomidov2002@gmail.com>
2023-08-15 10:57:37 +0500

    some changes

--- src/components/BaseSmallCard/index.vue
+++ src/components/BaseSmallCard/index.vue
@@ -10,14 +10,14 @@ const props = defineProps({
 <template>
   <div class="px-4">
     <router-link :to="itemInfo.path">
-      <div class="relative rounded-lg overflow-hidden cursor-pointer h-64 sm:w-64 w-[300px]">
+      <div class="relative rounded-lg  shadow-xl shadow-black-500 overflow-hidden cursor-pointer h-64 sm:w-64 w-[300px]">
         <img
           :src="itemInfo.url"
           class="bg-contain w-full object-cover  h-full bg-center hover:scale-110 transition"
           alt=""
         />
-        <div class="absolute w-full py-1 pl-3 bottom-0 bg-main">
-          <p class="text-white text-[15px]">{{ itemInfo.name }}</p>
+        <div class="absolute w-full py-4 pl-3 bottom-0 bg-iColor">
+          <p class="text-[#474747] font-semibold uppercase text-[15px]">{{ itemInfo.name }}</p>
         </div>
       </div>
     </router-link>

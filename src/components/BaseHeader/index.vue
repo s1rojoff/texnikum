46b294875258f5b645d92ba6424321d712705927
Sirojov Dilshod <87010216+SirojovDev@users.noreply.github.com>
2023-09-07 07:05:58 +0500

    logo changed in header and footer a litte redesign

--- src/components/BaseHeader/index.vue
+++ src/components/BaseHeader/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref } from 'vue'
+import { ref } from 'vue'
 import { storeToRefs } from 'pinia'
 import BaseIcon from '@/components/BaseIcon/index.vue'
 import { useBaseHeader } from '@/components/BaseHeader/composable'
@@ -28,16 +28,24 @@ storeToRefs(store)
 </script>
 <template>
   <div>
-    <marquee behavior="scroll" direction="left" scrollamount="10" class="absolute text-lg text-red-600 z-50">Сайт работает
-      в тестовом режиме</marquee>
+    <marquee
+      behavior="scroll"
+      direction="left"
+      scrollamount="10"
+      class="absolute text-lg text-red-600 z-50"
+      >Сайт работает в тестовом режиме</marquee
+    >
     <!-- Logo and  icons for social media -->
     <div class="bg-white hidden md:block">
       <div
         class="lg:px-24 2xl:px-0 md:px-7 lg:py-2 mx-auto container md:flex md:items-center md:justify-between"
       >
-        <router-link to="/" class="cursor-pointer" @click="store.$state.allMenus = false"
-          ><img src="/images/logo-header.png" class="w-32" alt=""
-        /></router-link>
+        <router-link to="/" class="cursor-pointer" @click="store.$state.allMenus = false">
+          <div class="w-fit flex justify-start items-center gap-1">
+            <BaseIcon name="logo"/>
+            <p class="text-main uppercase mt-1.5 font-semibold leading-4 text-xs">Toshkent temir yo'l <br> texnikumi</p>
+          </div>
+        </router-link>
         <div class="md:flex md:items-center md:justify-end md:gap-2">
           <BaseIcon
             v-for="(icon, index) in topIconName"
@@ -60,27 +68,50 @@ storeToRefs(store)
 
     <!-- Laptop view navbars -->
     <div class="bg-[#E2F0F9]">
-      <div class="container mx-auto py-3 flex md:px-7 lg:px-24 2xl:px-0 px-5 items-center justify-between md:py-2 lg:py-6">
-        <BaseIcon class="lg:w-7 lg:h-7 cursor-pointer md:w-5 md:-5" @click="toggleNavbar" name="dashboard" />
+      <div
+        class="container mx-auto py-3 flex md:px-7 lg:px-24 2xl:px-0 px-5 items-center justify-between md:py-2 lg:py-6"
+      >
+        <BaseIcon
+          class="lg:w-7 lg:h-7 cursor-pointer md:w-5 md:-5"
+          @click="toggleNavbar"
+          name="dashboard"
+        />
 
-        <div class="lg:text-lg relative uppercase lg:block hidden lg:font-normal xl:font-extrabold 2xl:text-[22px] text-main"
-          v-for="(link, index) in navLinks" :key="index" @mouseenter="toggleSubNav(index)"
-          @mouseleave="toggleSubNav(index)">
+        <div
+          class="lg:text-lg relative uppercase lg:block hidden lg:font-normal xl:font-extrabold 2xl:text-[22px] text-main"
+          v-for="(link, index) in navLinks"
+          :key="index"
+          @mouseenter="toggleSubNav(index)"
+          @mouseleave="toggleSubNav(index)"
+        >
           <p class="cursor-pointer">{{ link.name }}</p>
 
-          <div v-if="navLinks[index].visible && !store.$state.allMenus"
-            class="h-auto py-2 w-64 absolute px-4 text-start bg-bgColor drop-shadow-md">
-            <p v-for="(item, index) in link.subMenu" :key="index"
-              class="menu-style text-black cursor-pointer text-sm font-bold py-1.5 m-3">
-              <router-link v-slot="{ isExactActive }" v-if="item.name != 'Direktorga murojaat qilish'" :to="item.route">
+          <div
+            v-if="navLinks[index].visible && !store.$state.allMenus"
+            class="h-auto py-2 w-64 absolute px-4 text-start bg-bgColor drop-shadow-md"
+          >
+            <p
+              v-for="(item, index) in link.subMenu"
+              :key="index"
+              class="menu-style text-black cursor-pointer text-sm font-bold py-1.5 m-3"
+            >
+              <router-link
+                v-slot="{ isExactActive }"
+                v-if="item.name != 'Direktorga murojaat qilish'"
+                :to="item.route"
+              >
                 <p :class="{ 'text-main': isExactActive }">{{ item.name }}</p>
               </router-link>
               <a v-else target="_blank" :href="item.route">{{ item.name }}</a>
             </p>
           </div>
         </div>
-        <router-link to="/" class="cursor-pointer block md:hidden" @click="store.$state.allMenus = false"><img
-            src="/images/logo-header.png" class="w-32" alt="" /></router-link>
+        <router-link
+          to="/"
+          class="cursor-pointer block md:hidden"
+          @click="store.$state.allMenus = false"
+          ><img src="/images/logo-header.png" class="w-32" alt=""
+        /></router-link>
         <BaseIcon class="lg:w-7 lg:h-7 md:w-5 md:-5 hidden md:block" name="search" />
       </div>
     </div>
@@ -90,10 +121,17 @@ storeToRefs(store)
         <div class="flex w-full items-start gap-5 justify-center">
           <div v-for="(item, index) in navLinks" :key="index">
             <p class="text-lg font-semibold pb-2">{{ item.name }}</p>
-            <p v-for="(subItem, index) in item.subMenu" :key="index" @click="store.$state.allMenus = false"
-              class="text-sm py-1 cursor-pointer all-submenu">
-              <router-link v-slot="{ isExactActive }" :to="subItem.route"
-                v-if="subItem.name != 'Direktorga murojaat qilish'">
+            <p
+              v-for="(subItem, index) in item.subMenu"
+              :key="index"
+              @click="store.$state.allMenus = false"
+              class="text-sm py-1 cursor-pointer all-submenu"
+            >
+              <router-link
+                v-slot="{ isExactActive }"
+                :to="subItem.route"
+                v-if="subItem.name != 'Direktorga murojaat qilish'"
+              >
                 <p :class="{ 'text-main': isExactActive }">{{ subItem.name }}</p>
               </router-link>
               <a v-else target="_blank" :href="subItem.route">{{ subItem.name }}</a>
@@ -108,16 +146,29 @@ storeToRefs(store)
     <div v-if="store.$state.allMenus" class="block md:hidden bg-white w-full h-[100dvh] px-5">
       <div>
         <div v-for="(item, index) in navLinks" :key="index" class="pt-3">
-          <div @click.stop="handleClicked(item.id)" class="flex items-center cursor-pointer justify-start gap-5">
+          <div
+            @click.stop="handleClicked(item.id)"
+            class="flex items-center cursor-pointer justify-start gap-5"
+          >
             <p class="text-lg text-black font-medium">{{ item.name }}</p>
-            <BaseIcon :class="store.$state.phoneView === index + 1 ? 'rotate-180' : ''"
-              class="w-3 transition h-3 text-main" name="down" />
+            <BaseIcon
+              :class="store.$state.phoneView === index + 1 ? 'rotate-180' : ''"
+              class="w-3 transition h-3 text-main"
+              name="down"
+            />
           </div>
           <div class="ml-3" v-if="store.$state.phoneView === index + 1">
-            <p v-for="(subItem, subIndex) in item.subMenu" :key="subIndex" class="cursor-pointer block hover:text-main"
-              @click="store.$state.allMenus = false">
-              <router-link :to="subItem.route" v-if="subItem.name != 'Direktorga murojaat qilish'"
-                v-slot="{ isExactActive }">
+            <p
+              v-for="(subItem, subIndex) in item.subMenu"
+              :key="subIndex"
+              class="cursor-pointer block hover:text-main"
+              @click="store.$state.allMenus = false"
+            >
+              <router-link
+                :to="subItem.route"
+                v-if="subItem.name != 'Direktorga murojaat qilish'"
+                v-slot="{ isExactActive }"
+              >
                 <p :class="{ 'text-main': isExactActive }">{{ subItem.name }}</p>
               </router-link>
               <a v-else :href="subItem.route" target="_blank">

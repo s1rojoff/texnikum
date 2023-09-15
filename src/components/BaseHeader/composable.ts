65862bfe2302bd8d15f204749974308296c5c3ae
s1rojoff <dilshodcoder@gmail.com>
2023-09-15 09:18:48 +0500

    url added in header icons

--- src/components/BaseHeader/composable.ts
+++ src/components/BaseHeader/composable.ts
@@ -1,7 +1,23 @@
 import { ref } from "vue"
 
 export function useBaseHeader() {
-  const topIconName = ['instagram', 'facebook', 'lightbulb', 'telegram']
+  const topIconName = [
+    {
+      name: 'instagram',
+      url: 'https://instagram.com/ttytrailway?igshid=MzRlODBiNWFlZA=='
+    },
+    {
+      name: 'facebook',
+      url: ''
+    },
+    {
+      name: 'lightbulb',
+      url: ''
+    },
+    {
+      name: 'telegram',
+      url: ''
+    }]
   const locales = ref(['uz', 'ru'])
   return {
     topIconName,

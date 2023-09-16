6717e17968304db487d42b7c2c93ce7022089b10
unknown <you@example.comhusniddinxomidov2002@gmail.com>
2023-09-17 00:48:20 +0500

    some imges changed

--- src/components/BaseDepartmentCard/index.vue
+++ src/components/BaseDepartmentCard/index.vue
@@ -11,11 +11,11 @@ const props = defineProps({
 })
 </script>
 <template>
-  <div class="shadow-lg flex flex-col justify-center items-center p-5 sm:p-0 sm:flex-row  shadow-indigo-500/40 w-full h-auto  relative mb-5">
-    <div class=" h-full w-1/3">
+  <div class="shadow-lg flex flex-col justify-start gap-10 items-center p-5 sm:p-0 sm:flex-row  shadow-indigo-500/40 w-full h-auto  relative mb-5">
+    <div class=" h-full text-start w-1/4">
       <img class="object-cover w-full h-full" :src="props.cardData.url" alt="">
     </div>
-    <div class="w-2/3 ml-6 flex flex-col gap-4 mt-5">
+    <div class="w-2/4 ml-6 flex flex-col gap-4 mt-5">
       <p class="lg:text-xl sm:text-md text-sm uppercase border-l pl-5 border-l-indigo-500">{{ props.cardData.lavozim }}</p>
       <p class="lg:text-2xl sm:text-md text-sm font-bold text-main border-l pl-5 border-l-indigo-500">{{ props.cardData.name }}</p>
       <p class="lg:text-lg sm:text-md text-sm border-l pl-5 border-l-indigo-500">&phone; {{ props.cardData.phone }}</p>
